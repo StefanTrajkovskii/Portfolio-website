c700ca8dc20b29eda0d4717ede7f8e01f601167f
Stif <160525543+StefanTrajkovskii@users.noreply.github.com>
2025-02-14 22:18:55 +0100

    added dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 tailwind.config = {
+    darkMode: 'class',
     theme: {
         extend: {
             fontFamily: {
                 'bubblegum': ['"Bubblegum Sans"', 'cursive'],
                 'poppins': ['Poppins', 'sans-serif'],
             },
+            animation: {
+                'blob': 'blob 7s infinite',
+            },
+            keyframes: {
+                blob: {
+                    '0%': { transform: 'translateY(0px)' },
+                    '50%': { transform: 'translateY(-20px)' },
+                    '100%': { transform: 'translateY(0px)' },
+                }
+            }
         },
     },
 }
